b1927127a7fbfe564d14b4ad17b47be0a478b84d
Iván López <lopez.ivan@gmail.com>
2024-08-05 09:10:06 +0200

    Initial commit

---
+++ src/main/java/com/example/jobrunr_filter/JobrunrFilterApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobrunr_filter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobrunrFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobrunrFilterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jobrunr_filter/JobrunrFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobrunr_filter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobrunrFilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
